c4cf171f96c5bea6b919e7001ef791ba3101e788
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-07 11:14:28 +0300

    i wrote the code about the local storage when the page is refreshed it needs to be the same

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const inputEl = document.querySelector(".input");
 
 const bodyEl = document.querySelector("body");
 
-inputEl.checked = true;
+inputEl.checked = JSON.parse(localStorage.getItem
+("mode"));
+
+updateBody();
 
 function updateBody() {
     if (inputEl.checked) {
@@ -11,4 +14,13 @@ function updateBody() {
     else {
         bodyEl.style.background = "white";
     }
+}
+
+inputEl.addEventListener("input", () => {
+    updateBody();
+    updateLocalStorage();
+})
+
+function updateLocalStorage() {
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked))
 }
